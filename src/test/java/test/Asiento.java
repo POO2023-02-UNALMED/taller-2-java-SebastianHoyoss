42f0f0f6c7fcbb4b404eba2f74dd090db52f8682
SebastianHoyoss <jhoyosc@unal.edu.co>
2023-08-18 10:07:12 -0500

    asiento completo

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -6,6 +6,16 @@ public class Asiento {
 	int registro;
 	
 	void cambiarColor(String color) {
-		
+		if(color=="rojo") {
+			this.color=color;
+		}else if (color=="blanco") {
+			this.color=color;
+		}else if (color=="verde") {
+			this.color=color;
+		}else if (color=="amarillo") {
+			this.color=color;
+		}else if (color=="negro") {
+			this.color=color;
+		}
 	}
 }
