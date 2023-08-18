e60ba479da589f387ca1e9f0fda21f1b5bcf7fc5
SebastianHoyoss <jhoyosc@unal.edu.co>
2023-08-18 10:04:16 -0500

    motor java completo

--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -6,10 +6,15 @@ public class Motor {
 	int registro;
 	
 	void cambiarRegistro(int registro) {
-		
+		this.registro = registro;
 	}
 	void asignarTipo(String tipo) {
-		
-	}
-	
+		if (tipo== "electrico") {
+			this.tipo = tipo;
+		}else if (tipo=="gasolina") {
+				this.tipo=tipo;
+			}
+		}
 }
+	
+
